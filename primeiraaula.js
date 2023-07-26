9f48cfbcf62e6951d1bbffc85c99fb16dd17cd21
leticia almeida <23143052@edu119.local>
2023-07-26 14:27:19 -0300

    first commit

---
+++ primeiraaula.js
@@ -0,0 +1,57 @@
+
+/*atividade1*/
+console.log("Leticia Almeida")
+console.log('16')
+
+/*atividade2*/
+console.log("três semanas")
+
+/*atividade3*/
+
+/* 
+Olá, meu nome é letícia tenho 16 anos de idade
+e atualmente estou cursando desenvolvimento de sistemas
+*/
+
+/*atividade4*/
+let black= 90; //black agora é um numero
+let pink= "mesa"; // pink agora é uma string
+let white= false; // white agora é um booleano
+let brown= null; // brown agora é null
+let red= undefined; // red agora é undefined
+
+/*atividade5*/
+console.log (16 + 3,5)
+
+/*atividade6*/ 
+console.log(2023-1969)
+
+/*atividade7*/ 
+console.log(65/240)
+
+/*atividade8*/
+console.log(0.2708*100 )
+
+/*atividade9*/
+console.log('hello'+'world')
+
+/*atividade10*/
+console.log('hello ' + ' world')
+
+/*atividade11*/
+console.log('paralelepipedo'.length)
+
+/*atividade12*/
+console.log('Kaian Novais'.toUpperCase())
+
+/*atividade13*/
+console.log(' Professor Kaian Novais ' .trim())
+
+/*atividade14*/
+console.log(100* Math.random())
+
+/*atividade'5*/ 
+console.log(Math.ceil(43.8))
+
+/*atividade16*/
+console.log(Number.isInteger(2017))
